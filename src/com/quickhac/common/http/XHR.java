b639dcaf8ad1e62dc6a1b4e6b885a7bb999e3aed
Oksisane <ehsanmasdar@gmail.com>
2014-10-08 07:20:16 -0500

    Work on TEAMS sessions

--- src/com/quickhac/common/http/XHR.java
+++ src/com/quickhac/common/http/XHR.java
@@ -2,12 +2,14 @@ package com.quickhac.common.http;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
 
 import ch.boye.httpclientandroidlib.Consts;
+import ch.boye.httpclientandroidlib.Header;
 import ch.boye.httpclientandroidlib.HttpEntity;
 import ch.boye.httpclientandroidlib.HttpRequest;
 import ch.boye.httpclientandroidlib.HttpResponse;
@@ -29,27 +31,35 @@ import ch.boye.httpclientandroidlib.util.EntityUtils;
 /**
  * 
  * Prerequisites (if you want to mimic JavaScript's XHR behavior):
- *     DefaultHttpClient client = new VerifiedHttpClientFactory().getHttpClient();
- *     client.setRedirectStrategy(new XHR.RedirectStrategy());
+ * DefaultHttpClient client = new VerifiedHttpClientFactory().getHttpClient();
+ * client.setRedirectStrategy(new XHR.RedirectStrategy());
  *
  */
 public final class XHR {
-	
+
 	/**
 	 * Creates a new HTTP request and sends it.
-	 * @param client An HttpClient, so that state (esp. cookies) persist between
-	 * requests
-	 * @param method "GET" or "POST"
-	 * @param url the URL to send the request to
-	 * @param par a collection of parameters to send to the server via GET or POST
-	 * @param cb a success and a failure callback to call
+	 * 
+	 * @param client
+	 *            An HttpClient, so that state (esp. cookies) persist between
+	 *            requests
+	 * @param method
+	 *            "GET" or "POST"
+	 * @param url
+	 *            the URL to send the request to
+	 * @param par
+	 *            a collection of parameters to send to the server via GET or
+	 *            POST
+	 * @param cb
+	 *            a success and a failure callback to call
 	 */
 	public static void send(final HttpClient client, final String method,
 			final String url, final HashMap<String, String> par,
 			final ResponseHandler cb) {
 		// check method
 		if (method != "GET" && method != "POST")
-			throw new IllegalArgumentException("Unsupported HTTP request type: " + method);
+			throw new IllegalArgumentException(
+					"Unsupported HTTP request type: " + method);
 
 		// construct a URI
 		final URIBuilder builder;
@@ -59,7 +69,7 @@ public final class XHR {
 			cb.onFailure(e);
 			return;
 		}
-		
+
 		// make the request
 		final HttpUriRequest req;
 		// GET and POST require that parameters be put in different places in
@@ -86,15 +96,20 @@ public final class XHR {
 				final List<NameValuePair> params = new LinkedList<NameValuePair>();
 				for (Entry<String, String> p : par.entrySet())
 					params.add(new BasicNameValuePair(p.getKey(), p.getValue()));
-				final UrlEncodedFormEntity entity = new UrlEncodedFormEntity(params, Consts.UTF_8);
+				final UrlEncodedFormEntity entity = new UrlEncodedFormEntity(
+						params, Consts.UTF_8);
 				((HttpPost) req).setEntity(entity);
 				System.out.println("POST " + req.getURI());
 				System.out.print("     with form data: ");
-				try { entity.writeTo(System.out); } catch (IOException e) {}
+				try {
+					entity.writeTo(System.out);
+				} catch (IOException e) {
+				}
 				System.out.println();
+				
 			}
 		}
-		
+
 		// execute the request and try to get a response
 		final HttpResponse response;
 		try {
@@ -106,7 +121,6 @@ public final class XHR {
 			cb.onFailure(e);
 			return;
 		}
-		
 		// process the response
 		final HttpEntity responseEntity = response.getEntity();
 		final String responseString;
@@ -121,9 +135,189 @@ public final class XHR {
 		}
 		cb.onSuccess(responseString);
 	}
-	
-	public static class RedirectStrategy extends DefaultRedirectStrategy {            
-		public boolean isRedirected(final HttpRequest request, final HttpResponse response, final HttpContext context)  {
+	/**
+	 * Creates a new HTTP request and sends it.
+	 * 
+	 * @param client
+	 *            An HttpClient, so that state (esp. cookies) persist between
+	 *            requests
+	 * @param method
+	 *            "GET" or "POST"
+	 * @param url
+	 *            the URL to send the request to
+	 * @param par
+	 *            a collection of parameters to send to the server via GET or
+	 *            POST
+	 * @param cookie
+	 *            Optional cookie(s) to send with POST request
+	 * @param cb
+	 *            a success and a failure callback to call
+	 */
+	public static void sendwithCookie(final HttpClient client, final String method,
+			final String url, final HashMap<String, String> par,String cookies,
+			final ResponseHandler cb) {
+		// check method
+		if (method != "GET" && method != "POST")
+			throw new IllegalArgumentException(
+					"Unsupported HTTP request type: " + method);
+
+		// construct a URI
+		final URIBuilder builder;
+		try {
+			builder = new URIBuilder(url);
+		} catch (URISyntaxException e) {
+			cb.onFailure(e);
+			return;
+		}
+
+		// make the request
+		final HttpUriRequest req;
+		// GET and POST require that parameters be put in different places in
+		// the request.
+		if (method == "GET") {
+			if (par != null)
+				for (Entry<String, String> p : par.entrySet())
+					builder.setParameter(p.getKey(), p.getValue());
+			try {
+				req = new HttpGet(builder.build());
+			} catch (URISyntaxException e) {
+				cb.onFailure(e);
+				return;
+			}
+			System.out.println("GET " + req.getURI());
+		} else {
+			try {
+				req = new HttpPost(builder.build());
+			} catch (URISyntaxException e) {
+				cb.onFailure(e);
+				return;
+			}
+			if (par != null) {
+				final List<NameValuePair> params = new LinkedList<NameValuePair>();
+				for (Entry<String, String> p : par.entrySet())
+					params.add(new BasicNameValuePair(p.getKey(), p.getValue()));
+				final UrlEncodedFormEntity entity = new UrlEncodedFormEntity(
+						params, Consts.UTF_8);
+				((HttpPost) req).setEntity(entity);
+				System.out.println("POST " + req.getURI());
+				System.out.print("     with form data: ");
+				try {
+					entity.writeTo(System.out);
+				} catch (IOException e) {
+				}
+				System.out.println();
+			
+			}
+			if (cookies != null ){
+				req.setHeader("Cookie", cookies);
+				req.setHeader("Cache-Control", "no-cache");
+				req.setHeader("Content-Type", "application/x-www-form-urlencoded");
+			}
+			System.out.println(Arrays.toString(req.getAllHeaders()));
+		}
+
+		// execute the request and try to get a response
+		final HttpResponse response;
+		try {
+			response = client.execute(req);
+		} catch (ClientProtocolException e) {
+			cb.onFailure(e);
+			return;
+		} catch (IOException e) {
+			cb.onFailure(e);
+			return;
+		}
+		// process the response
+		final HttpEntity responseEntity = response.getEntity();
+		final String responseString;
+		try {
+			responseString = EntityUtils.toString(responseEntity);
+		} catch (ParseException e) {
+			cb.onFailure(e);
+			return;
+		} catch (IOException e) {
+			cb.onFailure(e);
+			return;
+		}
+		cb.onSuccess(responseString);
+	}
+	/**
+	 * Creates a new HTTP request and sends it,then returns any Set-Cookie
+	 * responses.
+	 * 
+	 * @param client
+	 *            An HttpClient, so that state (esp. cookies) persist between
+	 *            requests
+	 * @param url
+	 *            the URL to send the request to
+	 * @param par
+	 *            a collection of parameters to send to the server via POST
+	 * @param cookie
+	 *            optional cookies to send with the request
+	 */
+	public static Header[] getCookie(final HttpClient client, final String url,
+			final HashMap<String, String> par, String cookie) {
+		// construct a URI
+		final URIBuilder builder;
+		try {
+			builder = new URIBuilder(url);
+		} catch (URISyntaxException e) {
+			return null;
+		}
+
+		// make the request
+		final HttpUriRequest req;
+		// GET and POST require that parameters be put in different places in
+		// the request.
+
+		try {
+			req = new HttpPost(builder.build());
+		} catch (URISyntaxException e) {
+			return null;
+		}
+		if (par != null) {
+			final List<NameValuePair> params = new LinkedList<NameValuePair>();
+			for (Entry<String, String> p : par.entrySet())
+				params.add(new BasicNameValuePair(p.getKey(), p.getValue()));
+			final UrlEncodedFormEntity entity = new UrlEncodedFormEntity(
+					params, Consts.UTF_8);
+			((HttpPost) req).setEntity(entity);
+			System.out.println("POST " + req.getURI());
+			System.out.print("     with form data: ");
+			try {
+				entity.writeTo(System.out);
+			} catch (IOException e) {
+			}
+			System.out.println();
+			req.setHeader("Cookie", cookie);
+		}
+
+		// execute the request and try to get a response
+		final HttpResponse response;
+		try {
+			response = client.execute(req);
+		} catch (ClientProtocolException e) {
+			return null;
+		} catch (IOException e) {
+			return null;
+		}
+		// process the response
+		final HttpEntity responseEntity = response.getEntity();
+		final String responseString;
+		try {
+			responseString = EntityUtils.toString(responseEntity);
+		} catch (ParseException e) {
+			return null;
+		} catch (IOException e) {
+			return null;
+		}
+		System.out.println(Arrays.toString(response.getAllHeaders()));
+		return response.getHeaders("Set-Cookie");
+	}
+
+	public static class RedirectStrategy extends DefaultRedirectStrategy {
+		public boolean isRedirected(final HttpRequest request,
+				final HttpResponse response, final HttpContext context) {
 			boolean isRedirect = false;
 			try {
 				isRedirect = super.isRedirected(request, response, context);
@@ -131,7 +325,8 @@ public final class XHR {
 				e.printStackTrace();
 			}
 			if (!isRedirect) {
-				final int responseCode = response.getStatusLine().getStatusCode();
+				final int responseCode = response.getStatusLine()
+						.getStatusCode();
 				if (responseCode == 301 || responseCode == 302) {
 					return true;
 				}
@@ -139,9 +334,10 @@ public final class XHR {
 			return isRedirect;
 		}
 	}
-	
+
 	public static abstract class ResponseHandler {
 		public abstract void onSuccess(String response);
+
 		public abstract void onFailure(Exception e);
 	}
 }
--- src/com/quickhac/common/test/Runner.java
+++ src/com/quickhac/common/test/Runner.java
@@ -1,4 +1,5 @@
 package com.quickhac.common.test;
+
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -9,65 +10,67 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.HashMap;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSocketFactory;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import ch.boye.httpclientandroidlib.Header;
+import ch.boye.httpclientandroidlib.impl.client.DefaultHttpClient;
 
 import com.quickhac.common.TEAMSGradeParser;
 import com.quickhac.common.data.ClassGrades;
+import com.quickhac.common.http.ASPNETPageState;
+import com.quickhac.common.http.VerifiedHttpClientFactory;
+import com.quickhac.common.http.XHR;
 
 public class Runner {
+
+
 	public static void main(String args[]) throws IOException {
-		final String user = "stac";
-		final String password = "stacstac";
-		final String query = "cn=" + user + "&%5Bpassword%5D=" + password;
-		
-		final String host = "my.austinisd.org";
-		final String path = "/WebNetworkAuth/";
-		
-		final Socket socket = SSLSocketFactory.getDefault().createSocket(host, 443);
-		final PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-		writer.println("POST " + path + " HTTP/1.1");
-		writer.println("Host: " + host);
-		writer.println("Accept: */*");
-		writer.println("User-Agent: QHAC");
-		writer.println("Content-Type: application/x-www-form-urlencoded");
-		writer.println("Content-Length: " + query.length());
-		writer.println();
-		writer.println(query);
-		writer.println();
-		writer.flush();
-		
-		String cstonecookie = null;
-		
-		String response = "";
-		
-		final BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		final char[] buffer = new char[1024];
-		int len = 0;
-        while ((len = reader.read(buffer)) > 0) {
-        	response += new String(buffer, 0, len);
-        	if (response.endsWith("\r\n\r\n")) {
-        		break;
-        	}
-        }
-        
-        for (String line: response.split("\n")) {
-        	if (line.startsWith("Set-Cookie: CStoneSessionID=")) {
-        		cstonecookie = line.substring(12);
-        	}
-        }
-        
-        if (cstonecookie == null) {
-        	System.out.println("No cookie received!");
-        	System.exit(1);
-        }
-        
-        System.out.println(cstonecookie);
-		
-//		String html = readFile("test.html");
-//		TEAMSGradeParser parser = new TEAMSGradeParser();
-//		ClassGrades c = parser.parseClassGrades(html,"",0,0);
+		// String html = readFile("test.html");
+		// TEAMSGradeParser parser = new TEAMSGradeParser();
+		// ClassGrades c = parser.parseClassGrades(html,"",0,0);
+		// Prompt user for username and password
+		final JLabel usernameLabel = new JLabel("Username:");
+		final JTextField usernameField = new JTextField();
+		final JLabel passwordLabel = new JLabel("Password:");
+		final JPasswordField passwordField = new JPasswordField();
+		JOptionPane.showConfirmDialog(null, new Object[] { usernameLabel,
+				usernameField, passwordLabel, passwordField }, "Login",
+				JOptionPane.OK_CANCEL_OPTION);
+
+		final String AISDuser = usernameField.getText();
+		final String AISDpass = new String(passwordField.getPassword());
+		//String cstonecookie = getAustinisdCookie(AISDuser, AISDpass);
+		//String teamscookie = getTEAMSCookie(AISDuser, AISDpass, cstonecookie);
+		DefaultHttpClient client = new VerifiedHttpClientFactory().getNewHttpClient();
+		final XHR.ResponseHandler getGradesPage = new XHR.ResponseHandler() {
+
+			@Override
+			public void onSuccess(String response) {
+				//System.out.println(response);
+			}
+
+			@Override
+			public void onFailure(Exception e) {
+			}
+
+		};
+		//String finalcookie = teamscookie + ';' + cstonecookie;
+		//System.out.println(finalcookie);	
+		String gradeBookKey = "gradeBookKey=sectionIndex%253D2%252CgradeTypeIndex%253D1st%2BSix%2BWeeks%252CcourseIndex%253D4435.P100.Y%252CcalendarIndex%253D1%252CgradeIndex%253D95%252CteacherIndex%253DStormberg%255E%2BJohn%252CdayCodeIndex%253DA%2B-%2B04%252ClocIndex%253D018";
+		String html = getTEAMSPage("/selfserve/PSSViewReportCardsAction.do", "", "");
+		System.out.println(html);
 	}
 
 	static String readFile(String fileName) throws IOException {
@@ -85,5 +88,116 @@ public class Runner {
 		} finally {
 			br.close();
 		}
+	}
+
+	public static String getAustinisdCookie(final String AISDuser,
+			final String AISDpass) throws UnknownHostException, IOException {
+		final String user = AISDuser;
+		final String password = AISDpass;
+		final String query = "cn=" + user + "&%5Bpassword%5D=" + password;
+
+		final String host = "my.austinisd.org";
+		final String path = "/WebNetworkAuth/";
+
+		final Socket socket = SSLSocketFactory.getDefault().createSocket(host,
+				443);
+		final PrintWriter writer = new PrintWriter(new OutputStreamWriter(
+				socket.getOutputStream()));
+		writer.println("POST " + path + " HTTP/1.1");
+		writer.println("Host: " + host);
+		writer.println("Accept: */*");
+		writer.println("User-Agent: QHAC");
+		writer.println("Content-Type: application/x-www-form-urlencoded");
+		writer.println("Content-Length: " + query.length());
+		writer.println();
+		writer.println(query);
+		writer.println();
+		writer.flush();
+
+		String cstonecookie = null;
+
+		String response = "";
+
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(
+				socket.getInputStream()));
+		final char[] buffer = new char[1024];
+		int len = 0;
+		while ((len = reader.read(buffer)) > 0) {
+			response += new String(buffer, 0, len);
+			if (response.endsWith("\r\n\r\n")) {
+				break;
+			}
+		}
+
+		for (String line : response.split("\n")) {
+			if (line.startsWith("Set-Cookie: CStoneSessionID=")) {
+				cstonecookie = line.substring(12);
+			}
+		}
+
+		if (cstonecookie == null) {
+			System.out.println("No cookie received!");
+			System.exit(1);
+		}
+
+		System.out.println(cstonecookie);
+		// Split on first semicolon but keep first semicolon
+		return cstonecookie.split(";")[0];
+	}
+
+	public static String getTEAMSCookie(final String AISDuser,
+			final String AISDpass, final String CStoneCookie)
+			throws UnknownHostException, IOException {
+		DefaultHttpClient client = new VerifiedHttpClientFactory().getNewHttpClient();
+		final XHR.ResponseHandler getGradesPage = new XHR.ResponseHandler() {
+
+			@Override
+			public void onSuccess(String response) {
+				//System.out.println(response);
+			}
+
+			@Override
+			public void onFailure(Exception e) {
+			}
+
+		};
+		HashMap<String, String> par = new HashMap<String, String>();
+		par.put("userLoginId", AISDuser);
+		par.put("userPassword", AISDpass);
+		Header[] cookiesToSet = XHR.getCookie(client,"https://my-teams.austinisd.org/selfserve/SignOnLoginAction.do",
+				par, CStoneCookie);
+		return cookiesToSet[0].getValue().split(";")[0];
+	}
+	public static String getTEAMSPage(final String path,
+			final String gradeBookKey, String cookie) throws UnknownHostException, IOException {
+		final String host = "my-teams.austinisd.org";
+		final Socket socket = SSLSocketFactory.getDefault().createSocket(host,
+				443);
+		final PrintWriter writer = new PrintWriter(new OutputStreamWriter(
+				socket.getOutputStream()));
+		writer.println("POST " + path + " HTTP/1.1");
+		writer.println("Host: " + host);
+		writer.println("Cookie: " + cookie);
+		writer.println("Cache-Control: no-cache");
+		writer.println("Content-Type: application/x-www-form-urlencoded");
+		writer.println();
+		writer.println(gradeBookKey);
+		writer.println();
+		writer.flush();
+
+		String response = "";
+
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(
+				socket.getInputStream()));
+		final char[] buffer = new char[1024];
+		int len = 0;
+		while ((len = reader.read(buffer)) > 0) {
+			response += new String(buffer, 0, len);
+			if (response.endsWith("\r\n\r\n")) {
+				break;
+			}
+		}
+
+		return response;
 	}
 }
