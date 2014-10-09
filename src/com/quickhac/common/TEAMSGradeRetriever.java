cf98cefc0207617fb7407cb57fb13bc0b6906037
Ehsan Asdar <ehsanmasdar@gmail.com>
2014-10-09 11:05:54 -0500

    Break out http lib

--- src/com/quickhac/common/TEAMSGradeRetriever.java
+++ src/com/quickhac/common/TEAMSGradeRetriever.java
@@ -1,7 +1,15 @@
 package com.quickhac.common;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.HashMap;
+
+import javax.net.ssl.SSLSocketFactory;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -24,210 +32,112 @@ import com.quickhac.common.http.VerifiedHttpClientFactory;
 import com.quickhac.common.http.XHR;
 
 public class TEAMSGradeRetriever {
-	
-	final DefaultHttpClient client;
-	final GradeSpeedDistrict district;
-	
-	/**
-	 * Creates a new grade retriever for the specified district.
-	 */
-	public TEAMSGradeRetriever(final GradeSpeedDistrict d) {
-		client = new VerifiedHttpClientFactory().getNewHttpClient();
-		client.setRedirectStrategy(new XHR.RedirectStrategy());
-		district = d;
-	}
-	
-	/**
-	 * Logs into a district grades website with the given credentials and
-	 * specified callback. 
-	 */
-	public void login(final String user, final String pass, final LoginResponseHandler handler) {
-		final XHR.ResponseHandler getDisambigChoices = new XHR.ResponseHandler() {
-
-			@Override
-			public void onSuccess(String response) {
-				final Document doc = Jsoup.parse(response);
-				if (!district.isValidOutput(doc)) {
-					handler.onFailure(new InvalidGradeSpeedOutputException());
-					return;
-				}
-				
-				final ASPNETPageState state = ASPNETPageState.parse(doc);
-				
-				if (district.requiresDisambiguation(doc)) {
-					
-					// load the picker if necessary
-					if (district.disambiguatePickerLoadsFromAjax()) {
-						final XHR.ResponseHandler pickerLoaded = new XHR.ResponseHandler() {
-							@Override
-							public void onSuccess(String response) {
-								final Document doc = Jsoup.parse(response);
-								// don't check for valid GradeSpeed output
-								final StudentInfo[] choices = district.getDisambiguationChoices(doc);
-								handler.onRequiresDisambiguation(response, choices, state);
-							}
-							@Override
-							public void onFailure(Exception e) {
-								handler.onFailure(e);
-							}
-						};
-						XHR.send(client, "GET", district.disambiguateURL(), null, pickerLoaded);
-					} else {
-						final StudentInfo[] choices = district.getDisambiguationChoices(doc);
-						handler.onRequiresDisambiguation(response, choices, state);
-					}
-					
-				} else handler.onDoesNotRequireDisambiguation(response);
-			}
-
-			@Override
-			public void onFailure(Exception e) {
-				handler.onFailure(e);
-			}
-			
-		};
+	public static String getAustinisdCookie(final String AISDuser,
+			final String AISDpass) throws UnknownHostException, IOException {
+		final String query = "cn=" + AISDuser + "&%5Bpassword%5D=" + AISDpass;
 		
-		final XHR.ResponseHandler doLogin = new XHR.ResponseHandler() {
-
-			@Override
-			public void onSuccess(String response) {
-				final Document doc = Jsoup.parse(response);
-				final ASPNETPageState state = ASPNETPageState.parse(doc);
-				final HashMap<String, String> query = district.makeLoginQuery(user, pass, state);
-				XHR.send(client, district.loginMethod(), district.loginURL(), query, getDisambigChoices);
-			}
-
-			@Override
-			public void onFailure(Exception e) {
-				handler.onFailure(e);
-			}
-			
-		};
+		final String response = postPageHTTPS("my.austinisd.org", "/WebNetworkAuth/", new String[]{
+				"User-Agent: QHAC",
+				"Accept: */*"
+		}, query);
 		
-		XHR.send(client, "GET", district.loginURL(), null, doLogin);
-	}
-	
-	public void disambiguate(final String studentId, final ASPNETPageState state, final XHR.ResponseHandler handler) {
-		HashMap<String, String> query = district.makeDisambiguateQuery(studentId, state);
-		
-		XHR.send(client, district.disambiguateMethod(), district.disambiguateURL(), query, makeValidatedHandler(handler));
-	}
-	
-	public void getAverages(final XHR.ResponseHandler handler) {
-		XHR.send(client, district.gradesMethod(), district.gradesURL(), null, makeValidatedHandler(handler));
-	}
-	
-	public void getCycle(final String urlHash, final Document gradesPage, final XHR.ResponseHandler handler) {
-		ASPNETPageState state = null;
-		
-		// parse grades page if necessary
-		if (district.cycleGradesRequiresAveragesLoaded())
-			if (gradesPage == null)
-				throw new IllegalArgumentException("District requires that averages be loaded before cycle grades can be loaded but no averages page was passed.");
-			else
-				state = ASPNETPageState.parse(gradesPage);
-		
-		final HashMap<String, String> query = district.makeCycleQuery(urlHash, state);
-		XHR.send(client, district.cycleMethod(), district.cycleURL(), query, makeValidatedHandler(handler));
-	}
-	
-	private XHR.ResponseHandler makeValidatedHandler(final XHR.ResponseHandler handler) {
-		return new XHR.ResponseHandler() {
+		String cstonecookie = null;
 
-			@Override
-			public void onSuccess(String response) {
-				Document doc = Jsoup.parse(response);
-				if (!district.isValidOutput(doc)) {
-					handler.onFailure(new InvalidGradeSpeedOutputException());
-					return;
-				}
-				
-				handler.onSuccess(response);
+		for (String line : response.split("\n")) {
+			if (line.startsWith("Set-Cookie: CStoneSessionID=")) {
+				cstonecookie = line.substring(12);
 			}
-
-			@Override
-			public void onFailure(Exception e) {
-				handler.onFailure(e);
-			}
-			
-		};
-	}
-	
-	public static abstract class LoginResponseHandler {
-		public abstract void onRequiresDisambiguation(String response, StudentInfo[] students, ASPNETPageState state);
-		public abstract void onDoesNotRequireDisambiguation(String response);
-		public abstract void onFailure(Exception e);
-	}
-	public static String getGradesPage(String AISDuser, String AISDpass) {
-		try {
-			WebClient webClient = getTEAMSConnection(AISDuser, AISDpass);
-			// Load Teams
-			final HtmlPage page3 = webClient
-					.getPage("https://my-teams.austinisd.org/selfserve/PSSViewReportCardsAction.do");
-			System.out.println(page3.asText());
-			return page3.asXml();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
-		return null;
 
-	}
-
-	private static WebClient getTEAMSConnection(String AISDuser, String AISDpass) {
-		try {
-			// Extract jSessionId from startURL for Cookies
-			final WebClient webClient = new WebClient(BrowserVersion.CHROME);
-			WebClientOptions config = webClient.getOptions();
-			config.setThrowExceptionOnScriptError(false);
-			config.setCssEnabled(false);
-			cookieSet(webClient, AISDuser, AISDpass);
-
-			// Get Teams Login Page
-			HtmlPage page = webClient
-					.getPage("https://my-teams.austinisd.org/selfserve/EntryPointSignOnAction.do?parent=false");
-
-			// Get and fill out form
-			HtmlForm form = page.getFormByName("SmartForm");
-			HtmlTextInput username = form.getInputByName("userLoginId");
-			HtmlPasswordInput password = form.getInputByName("userPassword");
-			username.setValueAttribute(AISDuser);
-			password.setValueAttribute(AISDpass);
-
-			// Submit form
-			String javaScriptCode = "var form = (document.forms['SmartForm']); form.action = \"https://my-teams.austinisd.org/selfserve/SignOnLoginAction.do?parent=false\"; form.submit();";
-			page.executeJavaScript(javaScriptCode);
-			return webClient;
-		} catch (IOException e) {
-			e.printStackTrace();
+		if (cstonecookie == null) {
+			System.out.println("No cookie received!");
+			System.exit(1);
 		}
-		return null;
+
+		System.out.println(cstonecookie);
+		// Split on first semicolon
+		return cstonecookie.split(";")[0];
 	}
 
-	private static void cookieSet(final WebClient webClient,
-			final String AISDuser, final String AISDpass) {
-		try {
-			HtmlPage page = webClient.getPage("https://my.austinisd.org/");
-			HtmlForm form = page.getFormByName("loginForm");
-			HtmlTextInput username = form.getInputByName("cn");
-			HtmlPasswordInput password = form.getInputByName("[password]");
-			username.setValueAttribute(AISDuser);
-			password.setValueAttribute(AISDpass);
-			String javaScriptCode = "document.forms['loginForm'].submit();";
-			page.executeJavaScript(javaScriptCode);
+	public static String getTEAMSCookie(final String CStoneCookie)
+			throws UnknownHostException, IOException {
+		final String query = "";
+		final String response = postPageHTTPS("my-teams.austinisd.org", "/selfserve/EntryPointSignOnAction.do?parent=false", new String[]{
+				"Cookie: " + CStoneCookie,
+				"Accept: */*",
+				"User-Agent: QHAC"
+		}, query);
+		String jcookie = null;
 
-			boolean failure = true;
-			for (final Cookie cookie : webClient.getCookieManager()
-					.getCookies()) {
-				if (cookie.getName().equalsIgnoreCase("CSTONESESSIONID")) {
-					failure = false;
+		for (String line : response.split("\n")) {
+			if (line.startsWith("Set-Cookie: JSESSIONID=")) {
+				jcookie = line.substring(12);
+			}
+		}
+
+		if (jcookie == null) {
+			System.out.println("No cookie received!");
+			System.exit(1);
+		}
+		System.out.println(jcookie);
+		// Split on first semicolon
+		return jcookie.split(";")[0];
+	}
+
+	public static void postTEAMSLogin(final String AISDuser,
+			final String AISDpass, final String CStoneCookie)
+			throws UnknownHostException, IOException {
+		final String query = "userLoginId=" + AISDuser + "&userPassword=" + AISDpass;
+		final String response = postPageHTTPS("my-teams.austinisd.org", "/selfserve/SignOnLoginAction.do", new String[]{
+				"Cookie: " + CStoneCookie,
+				"Accept: */*",
+				"User-Agent: QHAC"
+		}, query);
+	}
+	
+	public static String getTEAMSPage(final String path,
+			final String gradeBookKey, String cookie) throws UnknownHostException, IOException {
+		return postPageHTTPS("my-teams.austinisd.org", path, new String[]{
+				"Cookie: " + cookie,
+				"Referer: https://my-teams.austinisd.org/selfserve/EntryPointSignOnAction.do?parent=false"
+		}, gradeBookKey);
+	}
+	
+	public static String postPageHTTPS(final String host, final String path, final String[] headers, final String postData) throws UnknownHostException, IOException {
+		final Socket socket = SSLSocketFactory.getDefault().createSocket(host,
+				443);
+		try {
+			final PrintWriter writer = new PrintWriter(new OutputStreamWriter(
+					socket.getOutputStream()));
+			writer.println("POST " + path + " HTTP/1.1");
+			writer.println("Host: " + host);
+			for (String header : headers) {
+				writer.println(header);
+			}
+			writer.println("Content-Length: " + postData.length());
+			writer.println("Content-Type: application/x-www-form-urlencoded");
+			writer.println();
+			writer.println(postData);
+			writer.println();
+			writer.flush();
+	
+			StringBuilder response = new StringBuilder();
+	
+			final BufferedReader reader = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+			final char[] buffer = new char[1024];
+			int len = 0;
+			while ((len = reader.read(buffer)) > 0) {
+				response.append(buffer, 0, len);
+				if (response.length() >= 4 && response.substring(response.length() - 4).equals("\r\n\r\n")) {
+					break;
 				}
 			}
-			if (failure) {
-				System.out.println("Incorrect username/password.");
-				System.exit(1);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+	
+			return response.toString();
+		} finally {
+			socket.close();
 		}
+		
 	}
 }
--- src/com/quickhac/common/test/Runner.java
+++ src/com/quickhac/common/test/Runner.java
@@ -21,6 +21,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import com.quickhac.common.TEAMSGradeParser;
+import com.quickhac.common.TEAMSGradeRetriever;
 import com.quickhac.common.data.ClassGrades;
 import com.quickhac.common.data.Course;
 
@@ -28,9 +29,6 @@ public class Runner {
 
 
 	public static void main(String args[]) throws IOException {
-		// String html = readFile("test.html");
-		// TEAMSGradeParser parser = new TEAMSGradeParser();
-		// ClassGrades c = parser.parseClassGrades(html,"",0,0);
 		// Prompt user for username and password
 		final JLabel usernameLabel = new JLabel("Username:");
 		final JTextField usernameField = new JTextField();
@@ -39,168 +37,39 @@ public class Runner {
 		JOptionPane.showConfirmDialog(null, new Object[] { usernameLabel,
 				usernameField, passwordLabel, passwordField }, "Login",
 				JOptionPane.OK_CANCEL_OPTION);
-		TEAMSGradeParser p = new TEAMSGradeParser();
-		final String AISDuser =  usernameField.getText();
-		final String AISDpass =  new String(passwordField.getPassword());
+		final String AISDuser =  URLEncoder.encode(usernameField.getText(), "UTF-8");
+		final String AISDpass =  URLEncoder.encode(new String(passwordField.getPassword()), "UTF-8");
+		
+		//////////////////////////////
+		
+		final TEAMSGradeParser p = new TEAMSGradeParser();
+		
 		//Get cookies
-		final String cstonecookie = getAustinisdCookie(AISDuser, AISDpass);
-		final String teamscookie = getTEAMSCookie(cstonecookie);
+		final String cstonecookie = TEAMSGradeRetriever.getAustinisdCookie(AISDuser, AISDpass);
+		final String teamscookie = TEAMSGradeRetriever.getTEAMSCookie(cstonecookie);
 		
 		//Generate final cookie
-		String finalcookie = teamscookie + ';' + cstonecookie;
-		System.out.println(finalcookie);	
+		final String finalcookie = teamscookie + ';' + cstonecookie;
 		
 		//POST to login to TEAMS
-		postTEAMSLogin(AISDuser,AISDpass,finalcookie);
+		TEAMSGradeRetriever.postTEAMSLogin(AISDuser,AISDpass,finalcookie);
 		
 		//Get "Report Card"
-		String averageHtml = getTEAMSPage("/selfserve/PSSViewReportCardsAction.do", "", finalcookie);
-		Course[] studentCourses = p.parseAverages(averageHtml);
-		System.out.println(averageHtml);
-		
-		//Parse averageHtml
-		for (Course c: studentCourses){
-			System.out.println("Teacher: " + c.teacherName + " Class: " + c.title);
-		}
+		final String averageHtml = TEAMSGradeRetriever.getTEAMSPage("/selfserve/PSSViewReportCardsAction.do", "", finalcookie);
+		final Course[] studentCourses = p.parseAverages(averageHtml);
 		
 		/*Logic to get ClassGrades. TEAMS looks for a post request with the "A" tag id of a specific grade selected, 
 		 * so we iterate through all the a tags we got above and send/store the parsed result one by one*/
-		ArrayList<ClassGrades> classGrades = new ArrayList<ClassGrades>();
-		Elements avalues = Jsoup.parse(averageHtml).getElementById("finalTablebottomRight1").getElementsByTag("a");
-		for (Element e: avalues){
-			if (isNumeric(e.text())){
+		final ArrayList<ClassGrades> classGrades = new ArrayList<ClassGrades>();
+		final Elements avalues = Jsoup.parse(averageHtml).getElementById("finalTablebottomRight1").getElementsByTag("a");
+		for (Element e: avalues) {
+			if (isNumeric(e.text())) {
 				String gradeBookKey = "selectedIndexId=-1&smartFormName=SmartForm&gradeBookKey=" + URLEncoder.encode(e.id(), "UTF-8");
-				classGrades.add( p.parseClassGrades(getTEAMSPage("/selfserve/PSSViewGradeBookEntriesAction.do", gradeBookKey, finalcookie), "", 0, 0));
+				classGrades.add( p.parseClassGrades(TEAMSGradeRetriever.getTEAMSPage("/selfserve/PSSViewGradeBookEntriesAction.do", gradeBookKey, finalcookie), "", 0, 0));
 			}
 		}
-		System.out.println("Success");
-	}
-
-	static String readFile(String fileName) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(fileName));
-		try {
-			StringBuilder sb = new StringBuilder();
-			String line = br.readLine();
-
-			while (line != null) {
-				sb.append(line);
-				sb.append("\n");
-				line = br.readLine();
-			}
-			return sb.toString();
-		} finally {
-			br.close();
-		}
-	}
-
-	public static String getAustinisdCookie(final String AISDuser,
-			final String AISDpass) throws UnknownHostException, IOException {
-		final String query = "cn=" + AISDuser + "&%5Bpassword%5D=" + AISDpass;
-		
-		final String response = postPageHTTPS("my.austinisd.org", "/WebNetworkAuth/", new String[]{
-				"User-Agent: QHAC",
-				"Accept: */*"
-		}, query);
-		
-		String cstonecookie = null;
-
-		for (String line : response.split("\n")) {
-			if (line.startsWith("Set-Cookie: CStoneSessionID=")) {
-				cstonecookie = line.substring(12);
-			}
-		}
-
-		if (cstonecookie == null) {
-			System.out.println("No cookie received!");
-			System.exit(1);
-		}
-
-		System.out.println(cstonecookie);
-		// Split on first semicolon
-		return cstonecookie.split(";")[0];
-	}
-
-
-	public static String getTEAMSCookie(final String CStoneCookie)
-			throws UnknownHostException, IOException {
-		final String query = "";
-		final String response = postPageHTTPS("my-teams.austinisd.org", "/selfserve/EntryPointSignOnAction.do?parent=false", new String[]{
-				"Cookie: " + CStoneCookie,
-				"Accept: */*",
-				"User-Agent: QHAC"
-		}, query);
-		String jcookie = null;
-
-		for (String line : response.split("\n")) {
-			if (line.startsWith("Set-Cookie: JSESSIONID=")) {
-				jcookie = line.substring(12);
-			}
-		}
-
-		if (jcookie == null) {
-			System.out.println("No cookie received!");
-			System.exit(1);
-		}
-		System.out.println(jcookie);
-		// Split on first semicolon
-		return jcookie.split(";")[0];
-	}
-
-	public static void postTEAMSLogin(final String AISDuser,
-			final String AISDpass, final String CStoneCookie)
-			throws UnknownHostException, IOException {
-		final String query = "userLoginId=" + AISDuser + "&userPassword=" + AISDpass;
-		final String response = postPageHTTPS("my-teams.austinisd.org", "/selfserve/SignOnLoginAction.do", new String[]{
-				"Cookie: " + CStoneCookie,
-				"Accept: */*",
-				"User-Agent: QHAC"
-		}, query);
-	}
-	public static String getTEAMSPage(final String path,
-			final String gradeBookKey, String cookie) throws UnknownHostException, IOException {
-		return postPageHTTPS("my-teams.austinisd.org", path, new String[]{
-				"Cookie: " + cookie,
-				"Referer: https://my-teams.austinisd.org/selfserve/EntryPointSignOnAction.do?parent=false"
-		}, gradeBookKey);
 	}
 	
-	public static String postPageHTTPS(final String host, final String path, final String[] headers, final String postData) throws UnknownHostException, IOException {
-		final Socket socket = SSLSocketFactory.getDefault().createSocket(host,
-				443);
-		try {
-			final PrintWriter writer = new PrintWriter(new OutputStreamWriter(
-					socket.getOutputStream()));
-			writer.println("POST " + path + " HTTP/1.1");
-			writer.println("Host: " + host);
-			for (String header : headers) {
-				writer.println(header);
-			}
-			writer.println("Content-Length: " + postData.length());
-			writer.println("Content-Type: application/x-www-form-urlencoded");
-			writer.println();
-			writer.println(postData);
-			writer.println();
-			writer.flush();
-	
-			StringBuilder response = new StringBuilder();
-	
-			final BufferedReader reader = new BufferedReader(new InputStreamReader(
-					socket.getInputStream()));
-			final char[] buffer = new char[1024];
-			int len = 0;
-			while ((len = reader.read(buffer)) > 0) {
-				response.append(buffer, 0, len);
-				if (response.length() >= 4 && response.substring(response.length() - 4).equals("\r\n\r\n")) {
-					break;
-				}
-			}
-	
-			return response.toString();
-		} finally {
-			socket.close();
-		}
-		
-	}
 	public static boolean isNumeric(String str)  
 	{  
 	  try  
@@ -214,22 +83,3 @@ public class Runner {
 	  return true;  
 	}
 }
-//DefaultHttpClient client = new VerifiedHttpClientFactory().getNewHttpClient();
-//final XHR.ResponseHandler getGradesPage = new XHR.ResponseHandler() {
-//
-//	@Override
-//	public void onSuccess(String response) {
-//		//System.out.println(response);
-//	}
-//
-//	@Override
-//	public void onFailure(Exception e) {
-//	}
-//
-//};
-//HashMap<String, String> par = new HashMap<String, String>();
-//par.put("userLoginId", AISDuser);
-//par.put("userPassword", AISDpass);
-//Header[] cookiesToSet = XHR.getCookie(client,"https://my-teams.austinisd.org/selfserve/SignOnLoginAction.do",
-//		par, CStoneCookie);
-//return cookiesToSet[0].getValue().split(";")[0];
